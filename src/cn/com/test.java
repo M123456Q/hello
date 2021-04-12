f9568f3c2970f1e85e40646c4f754ddb5157dc5c
MQ <3064657280@qq.com>
2021-04-12 21:39:31 +0800

    第er次

---
+++ src/cn/com/test.java
@@ -0,0 +1,12 @@
+package cn.com;
+
+/**
+ * @author MQ
+ * @date test年04月12日  21:38
+ */
+public class test {
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
