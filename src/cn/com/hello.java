f184c75f19f13f345e54a3e20f57d26d1c8bf816
MQ <3064657280@qq.com>
2021-04-12 21:33:28 +0800

    第一次

---
+++ src/cn/com/hello.java
@@ -0,0 +1,14 @@
+package cn.com;
+
+/**
+ * @author MQ
+ * @date hello年04月12日  20:36
+ */
+public class hello {
+    public  static void main(String[] args){
+        System.out.println("hello");
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
